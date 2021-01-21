0b3a02d1c3d5c2a93bf507f622c1ecd81f60b698
ahardisty <aaron.hardisty@gmail.com>
2021-01-21 05:16:45 -0800

    adds exemplar

--- macros/orders_by_payment_method.sql
+++ macros/orders_by_payment_method.sql
@@ -1,45 +1,23 @@
 /* What if this is a dynamically updating list of payment methods? */
 /* pull out all current distinct values of payment_method */
 {% set get_payment_methods_sql %}
-SELECT
-    DISTINCT payment_method
-FROM
-    {{ ref('stg_payments') }}
-
-    {% endset %}
-
-    {% set results = run_query(get_payment_methods_sql) %}
-    {% if execute %}
-        {% set payment_methods = results.columns ['PAYMENT_METHOD'].values() %}
-    {% endif %}
-
-    WITH payments AS (
-        SELECT
-            *
-        FROM
-            {{ ref('stg_payments') }}
-    ),
-    pivoted AS (
-        SELECT
-            order_id,
-            {%- for payment_method in payment_methods %}
-                SUM(
-                    CASE
-                        WHEN payment_method = '{{ payment_method }}' THEN amount
-                        ELSE 0
-                    END
-                ) AS {{ payment_method }}
-                _amount
-
-                {%- if not loop.last -%},
-            {%- endif -%}
+    select distinct payment_method from {{ ref('stg_payments') }}
+{% endset %}
+{% set results = run_query(get_payment_methods_sql) %}
+{% if execute %}
+    {% set payment_methods = results.columns['PAYMENT_METHOD'].values() %}
+{% endif %}
+with payments as (
+    select * from {{ ref('stg_payments') }}
+),
+pivoted as (
+    select 
+        order_id,
+    {%- for payment_method in payment_methods %}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+    {%- if not loop.last -%}, {%- endif -%}
         {% endfor %}
-        FROM
-            payments
-        GROUP BY
-            1
-    )
-SELECT
-    *
-FROM
-    pivoted
+    from payments
+    group by 1
+)
+select * from pivoted
